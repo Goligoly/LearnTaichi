fb83059c0f2226ef03c77b6a0718840c86f1f0c9
Gaoliyang96 <648653259@qq.com>
2020-10-06 09:54:44 +0800

    质点间引力

--- gravity.py
+++ gravity.py
@@ -9,7 +9,8 @@ Radius = 10
 
 MASS = 20
 GRAVITY = ti.Vector([0, -400])
-dt = 0.01667
+dt = 0.005 #0.01667
+G = 10000
 
 particleMax = 256
 particleNum = ti.field(ti.i32, shape=())
@@ -19,15 +20,19 @@ f = ti.Vector(2, dt=ti.f32, shape=particleMax)
 
 
 @ti.kernel
-def step():
+def step():# %% [markdown]
     for i in range(particleNum[None]):
-        f[i] = GRAVITY*MASS
+        f[i] = ti.Vector([0,0])
+        for j in range(particleNum[None]):
+            d = (x[j]-x[i]).norm()
+            if i != j and d > Radius*2:
+                f[i] += (G*MASS*MASS/d **3) * (x[j]-x[i])
 
         v[i] += f[i]*dt/MASS
 
         if x[i][1] <= Radius:
             v[i][1] = 0
-            x[i][1] = Radius
+            x[i][1] = Radius+1
 
         x[i] += v[i]*dt
 
@@ -35,11 +40,11 @@ def step():
 @ti.kernel
 def newParticle(pos_x: ti.f32, pos_y: ti.f32):
     x[particleNum[None]] = ti.Vector([pos_x, pos_y])
-    v[particleNum[None]] = ti.Vector([0, 0])
+    v[particleNum[None]] = ti.Vector([0,0])
     particleNum[None] += 1
 
 
-gui = ti.GUI('Free Fall', res=(WIDTH, HEIGHT), background_color=0xffffff)
+gui = ti.GUI('Gravity', res=(WIDTH, HEIGHT), background_color=0xffffff)
 newParticle(256, 256)
 while True:
     for e in gui.get_events(ti.GUI.PRESS):
